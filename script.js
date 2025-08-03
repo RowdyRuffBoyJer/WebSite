8f2491e3fbae574de8535914a880460272cde2f7
Jeremy Apanah <jrapanah@gmail.com>
2025-08-02 22:15:06 -0400

    Fixed some stuff

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById('menu-button');
+const navMenu = document.getElementById('nav-menu');
+
+menuBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('hidden');
+});
+
+
