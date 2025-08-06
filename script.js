3270281a6a32056e499226a0a9975f3c4fcec967
Jeremy Apanah <jrapanah@gmail.com>
2025-08-06 01:50:20 -0400

    Fixed some stuff

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.getElementById("menu-button");
-    const navList = document.querySelector("nav ul");
-
-    menuButton.addEventListener("click", () => {
-        navList.classList.toggle("show-menu");
-    });
-});
